6b29491afa3b4a8bb000ed504b3bfc5d0f9f4475
Rahavi Ramesh <RRamesh@spartaglobal.com>
2019-12-05 12:03:48 +0000

    Cleaned up the functions and run file

--- bizz_and_zzuu_functions.py
+++ bizz_and_zzuu_functions.py
@@ -2,17 +2,24 @@
 
 
 def div_by_3(num):
-    if (num % 3) == 0:
-        return 'bizz'
+    return (num % 3) == 0
 
 def div_by_5(num):
-    if (num % 5) == 0:
-        return 'zzuu'
+    return (num % 5) == 0
 
 def div_by_3_and_5(num):
-    if (num % 3) ==0 and (num % 5) == 0:
-        return 'bizzzzuu'
+    return (num % 3) ==0 and (num % 5) == 0
 
 def not_div_by_3_or_5(num):
-    if(num % 3) != 0 and (num % 5) != 0:
+    return (num % 3) != 0 and (num % 5) != 0
+
+def test(user_input):
+    if div_by_3(user_input):
+        return 'bizz'
+    elif div_by_5(user_input):
+        return 'zzuu'
+    elif div_by_3_and_5(user_input):
+        return 'bizzzzuu'
+    elif not_div_by_3_or_5(user_input):
         return 'none'
+
--- bizz_and_zzuu_run.py
+++ bizz_and_zzuu_run.py
@@ -3,16 +3,11 @@ from bizz_and_zzuu_functions import *
 
 # Call the functions
 while True:
-    user_input = input('Please enter a number:')
-    if user_input == 'done':
+    user_input = int(input('Please enter a number:'))
+
+    if user_input == 0:
         break
-    num =int(user_input)
-    if num % 3 == 0 and num % 5 == 0:
-        print(div_by_3_and_5(num))
-    elif num % 3 ==0:
-        print(div_by_3(num))
-    elif num % 5 == 0:
-        print(div_by_5(num))
-    elif num % 3 != 0 and num % 5 != 0:
-        print(not_div_by_3_or_5(num))
-print('Thank you for playing')
+    if user_input != 0:
+        print(user_input)
+        print(test(user_input))
+print('Thanks for playing')
